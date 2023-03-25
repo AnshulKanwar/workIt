590984cafad6f0bf561fb84c95897172d45136a5
Anshul Kanwar <4n5hu1@protonmail.com>
2023-03-26 01:57:44 +0530

    add Workout screen

---
+++ src/components/Exercise.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import { Exercise as TExercise } from "../types";
+import { getExercise } from "../utils";
+import Card from "../ui/Card";
+
+interface ExerciseProps {
+  exercise: TExercise;
+}
+
+const Exercise = ({ exercise: { id, sets } }: ExerciseProps) => {
+  return (
+    <View style={styles.container}>
+      <Card>
+        <Text style={styles.exerciseText}>{getExercise(id)}</Text>
+        <FlatList
+          data={sets}
+          renderItem={({ item: { order, weight, reps } }) => (
+            <View style={styles.set}>
+              <Text>{order}</Text>
+              <Text>{weight} kg</Text>
+              <Text>x</Text>
+              <Text>{reps}</Text>
+            </View>
+          )}
+        />
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 12,
+  },
+  exerciseText: {
+    fontSize: 20,
+    fontWeight: "500",
+    marginBottom: 8,
+  },
+  set: {
+    flexDirection: "row",
+    gap: 5,
+  },
+});
+
+export default Exercise;
---
+++ src/screens/Workout.tsx
@@ -0,0 +1,59 @@
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { useSelector } from "react-redux";
+import { format, formatDuration } from "date-fns";
+import { selectWorkout } from "../features/workout/workoutSlice";
+import { RootState } from "../store";
+import { WorkoutScreenRouteProp } from "../types";
+import Card from "../ui/Card";
+import Container from "../ui/Container";
+import Label from "../ui/Label";
+import Exercise from "../components/Exercise";
+
+interface WorkoutProps {
+  route: WorkoutScreenRouteProp;
+}
+
+const Workout = ({ route }: WorkoutProps) => {
+  const { id } = route.params;
+
+  const workout = useSelector((state: RootState) => selectWorkout(state, id));
+  const { date, duration, exercises } = workout;
+
+  return (
+    <Container>
+      <View style={styles.container}>
+        <Card>
+          <Text style={styles.dateText}>{format(date, "PPp")}</Text>
+          <Label icon="clock" title={formatDuration(duration)} />
+        </Card>
+        <View>
+          <Text style={styles.numExercisesText}>{exercises.length} exercises</Text>
+          <FlatList
+            data={exercises}
+            renderItem={({ item }) => <Exercise exercise={item} />}
+          />
+        </View>
+      </View>
+    </Container>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 30,
+  },
+  dateText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  duration: {
+    flexDirection: "row",
+  },
+  numExercisesText: {
+    color: "#71717a",
+    marginBottom: 4,
+  }
+});
+
+export default Workout;
